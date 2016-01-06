a2d041df4ecc091fd4217510fc3888a168bdaa2a
Bradley Herbst <Bordwalk2000@gmail.com>
2016-01-06 08:33:20 -0600

    Ran and Worked
    Fixed a few things that were causing errors when running the scripts,
    manually running errors when when numbers were getting over 10.

--- Website File Renamer/Website File Renamer.ps1
+++ Website File Renamer/Website File Renamer.ps1
@@ -1,41 +1,49 @@
 ﻿$path= ""
 $URL = ""
+$ExerciseFiles = "Exercise Files"
 
 Set-Location $path
 
-$HTML = Invoke-WebRequest -Uri $URL 
+$HTML = Invoke-WebRequest -Uri $URL
 $Chapter = ($HTML.ParsedHtml.body.getElementsByTagName("a") | Where{$_.id -like 'chapter-title-*'}).innerText
 
-$chapterCount=0
-foreach ($Item in $Chapter) {
-    $item = [RegEx]::Replace($item, "[{0}]" -f ([RegEx]::Escape(-join [System.IO.Path]::GetInvalidFileNameChars())), '') 
-    $item = $Item -replace '^\d+. '
+if(!(Test-Path -Path $ExerciseFiles)){New-Item -ItemType directory -Path $ExerciseFiles}
+Get-ChildItem -Filter "*.zip" -Recurse | Move-Item -Destination $ExerciseFiles
 
-    If($chapterCount -le 9){$FolderName = "0"+ $chaptercount + ". " + $Item}
-    Else{$FolderName = $chapterCount + ". " + $Item}
+$ChapterCount=0
+foreach ($Item in $Chapter) {
+    $Item = [RegEx]::Replace($Item, "[{0}]" -f ([RegEx]::Escape(-join [System.IO.Path]::GetInvalidFileNameChars())), '') 
+    $Item = $Item -replace '^\d+. '
+
+    If($ChapterCount -le 9){$FolderName = "0" + $ChapterCount + ". " + $Item}
+    Else{$FolderName = [String]$ChapterCount + ". " + $Item}
     
-    if(!(Test-Path -Path $FolderName )){ Write-host "Creating Folder $FolderName"; New-Item -ItemType directory -Path $FolderName}
+    if(!(Test-Path -Path $FolderName)){New-Item -ItemType directory -Path $FolderName}
     
-    $titles = $HTML.ParsedHtml.body.getElementsByTagName("li") | Where{$_.id -eq "toc-chapter-$chaptercount"} | 
+    $Titles = $HTML.ParsedHtml.body.getElementsByTagName("li") | Where{$_.id -eq "toc-chapter-$ChapterCount"} | 
     foreach{($_.getElementsByTagName("a") | Where{$_.classname -eq 'video-cta'}).innerText}
     
-    $number=1
-    foreach($title in $titles) {
-        $title = [RegEx]::Replace($title, "[{0}]" -f ([RegEx]::Escape(-join [System.IO.Path]::GetInvalidFileNameChars())), '') 
+    $Number=1
+    foreach($Title in $Titles) {
+        $Title = [RegEx]::Replace($Title, "[{0}]" -f ([RegEx]::Escape(-join [System.IO.Path]::GetInvalidFileNameChars())), '') 
     
-        If($number -le 9){$itemNumber = "0"+ $number}
-        Else{$itemNumber = $chapterCount + ". " + $Item}
-        
-        $titleName = $FolderName.substring(0,2) + "_" + $itemNumber + "-" + $title.Trim() + ".mp4"
+        If($Number -le 9){$ItemNumber = "0"+ $Number}
+        Else{$ItemNumber = $Number}
+                 
+        $TitleName = $FolderName.substring(0,2) + "_" + $ItemNumber + "-" + $Title.Trim() + ".mp4"
         
         Get-ChildItem -Filter "*.mp4" -Recurse | 
-        Where-Object {$_.Name.Substring(7,5) -match $titleName.Substring(0,5)} |
-        Rename-Item -newName $titleName 
+        Where-Object {$_.Name.Substring(7,5) -match $TitleName.Substring(0,5)} |
+        Rename-Item -newName $TitleName 
         
-        Get-ChildItem -Filter "*.mp4" -Recurse | Where {$_.Name.Substring(0,5) -match $titleName.Substring(0,5)} |
+        Get-ChildItem -Filter "*.mp4" -Recurse | Where {$_.Name.Substring(0,5) -match $TitleName.Substring(0,5)} |
         Move-Item -Destination $FolderName
         
         $number++
     }
-    $chaptercount++
+    $ChapterCount++
 }
+
+Get-ChildItem -recurse | Where {$_.PSIsContainer -and @(Get-ChildItem -Lit $_.Fullname -r | 
+Where {!$_.PSIsContainer}).Length -eq 0} |
+Remove-Item -recurse
